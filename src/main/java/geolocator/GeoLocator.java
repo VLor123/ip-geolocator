3859e4e4f9b2d7a2baf3f2a87b719cff3f8a7783
Vastag Lőrinc <vastaglorinc@gmail.com>
2023-02-27 22:07:48 +0100

    geolocator modification

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -1,11 +1,16 @@
 package geolocator;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
 import feign.Feign;
 import feign.Param;
 import feign.RequestLine;
 import feign.gson.GsonDecoder;
+import feign.Logger;
+import feign.slf4j.Slf4jLogger;
 
 public interface GeoLocator {
+
 
     @RequestLine("GET")
     GeoLocation getGeoLocation();
@@ -13,10 +18,16 @@ public interface GeoLocator {
     @RequestLine("GET /{ipOrHostName}")
     GeoLocation getGeoLocation(@Param("ipOrHostName") String ipOrHostName);
 
+
+
     static GeoLocator newInstance() {
         return Feign.builder()
-            .decoder(new GsonDecoder())
-            .target(GeoLocator.class, "http://ip-api.com/json/");
+                .decoder(new GsonDecoder(new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                        .create()))
+                .logger(new Slf4jLogger())
+                .logLevel(Logger.Level.FULL)
+                .target(GeoLocator.class, "https://reallyfreegeoip.org/json/");
     }
 
 }
